e64cfebd8d2bb5afe090a39297a1bfc552d92d82
Dang <kieuvantruyen.3dfish@gmail.com>
2020-11-17 00:04:46 +0700

    this is second commit for develop

--- src/test/java/feature/RegisterAccount.feature
+++ src/test/java/feature/RegisterAccount.feature
@@ -1,4 +1,5 @@
 Feature: Register Account and Login
+
   Scenario: Register Account
     Given I open the Home page
     When I click on Visit here link
@@ -8,3 +9,23 @@ Feature: Register Account and Login
     And I enter the UserID and Password to the login form
     And I click on Login button
     Then I can see my UserID on the Dashboard
+
+
+  Scenario Outline: Register Account
+    Given I open the Home page
+    When I click on Visit here link
+    And I input my email to the Email ID field and Submit
+    Then I get the UserID and Password
+    When I back to Home page
+    And I enter the UserID and Password to the login form
+    When I click New Customer on the Left menu
+    And I see the Add New Customer screen
+    Then I add the user information
+      |Customer Name  | Date of Birth  |Address  |City  |State  | PIN | Mobile Number  | Email | Password |
+      |<Customer Name>| <Date of Birth>|<Address>|<City>|<State>|<PIN>| <Mobile Number>|<Email>|<Password>|
+    When I click the Submit button
+
+    Examples:
+    |Customer Name  | Date of Birth | Address         | City          | State | PIN       | Mobile Number | Email             | Password |
+    |Truyen kieu    | 10/10/1991    | 123 Ngo Tat To  | Ho Chi Minh   | HCM   | 12345654  | 09844434      | truyen01@yop.com  | 123454   |
+
--- target/site/cucumber-report-default/report.js
+++ target/site/cucumber-report-default/report.js
@@ -7,7 +7,7 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.scenario({
-  "line": 2,
+  "line": 3,
   "name": "Register Account",
   "description": "",
   "id": "register-account-and-login;register-account",
@@ -15,42 +15,42 @@ formatter.scenario({
   "keyword": "Scenario"
 });
 formatter.step({
-  "line": 3,
+  "line": 4,
   "name": "I open the Home page",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 4,
+  "line": 5,
   "name": "I click on Visit here link",
   "keyword": "When "
 });
 formatter.step({
-  "line": 5,
+  "line": 6,
   "name": "I input my email to the Email ID field and Submit",
   "keyword": "And "
 });
 formatter.step({
-  "line": 6,
+  "line": 7,
   "name": "I get the UserID and Password",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 7,
+  "line": 8,
   "name": "I back to Home page",
   "keyword": "When "
 });
 formatter.step({
-  "line": 8,
+  "line": 9,
   "name": "I enter the UserID and Password to the login form",
   "keyword": "And "
 });
 formatter.step({
-  "line": 9,
+  "line": 10,
   "name": "I click on Login button",
   "keyword": "And "
 });
 formatter.step({
-  "line": 10,
+  "line": 11,
   "name": "I can see my UserID on the Dashboard",
   "keyword": "Then "
 });
@@ -58,56 +58,56 @@ formatter.match({
   "location": "HomePageSteps.i_open_the_home_page()"
 });
 formatter.result({
-  "duration": 4848686451,
+  "duration": 5468866466,
   "status": "passed"
 });
 formatter.match({
   "location": "HomePageSteps.i_click_on_visit_here_link()"
 });
 formatter.result({
-  "duration": 1647964565,
+  "duration": 487577563,
   "status": "passed"
 });
 formatter.match({
   "location": "RegisterPageSteps.i_input_my_email_to_the_email_id_field_and_submit()"
 });
 formatter.result({
-  "duration": 2201107821,
+  "duration": 1967431269,
   "status": "passed"
 });
 formatter.match({
   "location": "RegisterPageSteps.i_get_the_userid_and_password()"
 });
 formatter.result({
-  "duration": 90886257,
+  "duration": 97596275,
   "status": "passed"
 });
 formatter.match({
   "location": "RegisterPageSteps.i_back_to_home_page()"
 });
 formatter.result({
-  "duration": 1769358701,
+  "duration": 1176977236,
   "status": "passed"
 });
 formatter.match({
   "location": "HomePageSteps.i_enter_the_userid_and_password_to_the_login_form()"
 });
 formatter.result({
-  "duration": 283972178,
+  "duration": 200117072,
   "status": "passed"
 });
 formatter.match({
   "location": "HomePageSteps.i_click_on_login_button()"
 });
 formatter.result({
-  "duration": 2985921900,
+  "duration": 1469391801,
   "status": "passed"
 });
 formatter.match({
   "location": "DashboardPageSteps.i_can_see_my_userid_on_the_dashboard()"
 });
 formatter.result({
-  "duration": 351539538,
+  "duration": 42990882,
   "status": "passed"
 });
 });
